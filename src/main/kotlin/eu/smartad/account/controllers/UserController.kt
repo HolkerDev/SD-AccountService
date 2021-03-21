0bc93efdacf7107dfe1a1a4a67c8665b6423a2a4
HolkerDev <nick.penkovskiy@gmail.com>
2021-03-21 21:41:53 +0100

    add test endpoint

--- src/main/kotlin/eu/smartad/account/controllers/UserController.kt
+++ src/main/kotlin/eu/smartad/account/controllers/UserController.kt
@@ -2,13 +2,15 @@ package eu.smartad.account.controllers
 
 import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class UserController {
     private val log = LoggerFactory.getLogger(UserController::class.java)
 
-    fun getUsers(): ResponseEntity<*> {
-        TODO("")
+    @GetMapping("/test")
+    fun check(): ResponseEntity<String> {
+        return ResponseEntity.ok("Test is fine. Account endpoint is working")
     }
 }
